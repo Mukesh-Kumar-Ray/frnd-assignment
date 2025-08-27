72e2d00747860cb523a3b223ce4a1a30c8a401b6
Mukesh Kumar <ray.mukesh.2002@gmail.com>
2025-08-27 05:50:47 +0530

    changes unchange

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./db/db.js";
 import uploadRoute from "./routes/upload.route.js";
 import truckRoute from "./routes/truck.route.js";
-import serverless from "serverless-http";
+//import serverless from "serverless-http";
 
 dotenv.config();
 connectDB();
@@ -14,5 +14,11 @@ app.use(express.json());
 app.use("/api/upload", uploadRoute);
 app.use("/api/trucks", truckRoute);
 
+const port=process.env.PORT || 5000
+console.log (process.env.PORT)
+app.listen(port,()=>{
+    console.log("server is running...")
+})
 
-export default serverless(app);
+
+//export default serverless(app);
